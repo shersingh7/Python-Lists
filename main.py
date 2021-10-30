f84ee2dae46fad8ad12b57596fd5fb75cd12e270
Davinder <66485246+shersingh7@users.noreply.github.com>
2021-10-30 09:15:07 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+## Largest numbers on list
+
+# numbers = [3,6,2,8,4,10]
+# max = numbers[0]
+#
+# for num in numbers:
+#     if max < num:
+#         max = num
+#
+# print(f'The biggest number is {max}')
+
+
+## 2d Arrays
+
+# matrix = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+#
+# for row in matrix:
+#     for item in row:
+#         print(item)
+
+
+## List methods
+
+# numbers = [5,2,1,7,8]
+# numbers.append(20) # To add numbers at the end
+# numbers.insert(0,10) # Will add number 10 at the index 0
+# numbers.insert(7) # Will remove 5 from the list
+# numbers.clear() # Will empty the list
+# numbers.pop() # Will remove the last number from the list
+# numbers2 = numbers.copy() # Will copy
+
+
+
+
